00af31d41171fb4b3caa7cef16742a409c2d393b
senghor.khen <senghor.khen@PNC.LAN>
2020-02-09 17:04:06 +0700

    challenge5

--- CHALLENGE/js/game.js
+++ CHALLENGE/js/game.js
@@ -1,47 +1,51 @@
 const PLAYDELAY = 2000;
-const IMAGE_FILD = ["1.png","2.png","3.png","4.png","5.png","6.png",];
+const IMAGE_FILD = ["1.png", "2.png", "3.png", "4.png", "5.png", "6.png",];
 
 document.querySelector('#resetButtonId').style.display = "none";
 
-function setHtml(text){
-    var recipt= document.querySelector("#resultId");
-     recipt.innerHTML= text;
- }
+function setHtml(text) {
+    var recipt = document.querySelector("#resultId");
+    recipt.innerHTML = text;
+}
 
 //get image
-function getimage(){
+function getimage() {
+    var arr = [];
     var results = "";
-for(let i= 0; i < 3; i++){
-  var image = IMAGE_FILD[i];
-  results +=`
-  <img src="images/${image}" width="100">
-  `;
-  }
-  return results;
+
+    for (var i = 0; i < 3; i++) {
+        var rand = Math.floor(Math.random() * 6);
+        arr.push(rand);
+        var result = IMAGE_FILD[(arr[i])];
+        results += `
+<img src="images/${result}" width="100">
+`;
+    }
+    return results;
 }
 
-function visability( element , isVisible){
-var result = isVisible ? "block": "none";
-document.querySelector("#" + element).style.display = result;
+function visability(element, isVisible) {
+    var result = isVisible ? "block" : "none";
+    document.querySelector("#" + element).style.display = result;
 
 }
-function onPlayClicked(){
+function onPlayClicked() {
     setHtml("WAIT!");
-   setTimeout(() => {
-    visability('playButtonId',false);
-    visability('resetButtonId',true);
-    setHtml(getimage());
-   }, PLAYDELAY); 
+    setTimeout(() => {
+        visability('playButtonId', false);
+        visability('resetButtonId', true);
+        setHtml(getimage());
+    }, PLAYDELAY);
 }
 
-function onResetClicked(){
-    visability('playButtonId',true);
-    visability('resetButtonId',false);
+function onResetClicked() {
+    visability('playButtonId', true);
+    visability('resetButtonId', false);
     //setHtml("you can play");
 }
 
 const buttonPlay = document.querySelector('#playButtonId');
-buttonPlay.addEventListener('click',onPlayClicked);
+buttonPlay.addEventListener('click', onPlayClicked);
 
 const buttonReset = document.querySelector('#resetButtonId');
-buttonReset.addEventListener('click',onResetClicked);
+buttonReset.addEventListener('click', onResetClicked);
